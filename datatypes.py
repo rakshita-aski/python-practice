270f5485918215d60d3a4f2f4cafd6ef255e1331
rakshita-aski <akshataaski2000@gmail.com>
2026-07-27 18:09:59 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,16 @@
+name = "Rakshita"
+age = 21
+cgpa = 8.03
+is_student = False
+
+print(name)
+print(type(name))
+
+print(age)
+print(type(age))
+
+print(cgpa)
+print(type(cgpa))
+
+print(is_student)
+print(type(is_student))
---
+++ variable.py
@@ -0,0 +1,6 @@
+student_name = "Rakshita"
+student_age = 21
+student_branch = "ECE"
+print(student_name)
+print(student_age)
+print(student_branch)
